a7012fcbea8ae9a78e86632e5ccef2ad6c358b45
friendsproject302-gif <friendsproject302@gmail.com>
2026-08-04 22:14:21 +0530

    Fix Kotlin syntax error for channelName in MainActivity.kt

--- android/app/src/main/kotlin/com/library/management/MainActivity.kt
+++ android/app/src/main/kotlin/com/library/management/MainActivity.kt
@@ -10,12 +10,12 @@ import io.flutter.plugin.common.MethodChannel
 import java.io.File
 
 class MainActivity : FlutterActivity() {
-    private const val CHANNEL = "com.library.management/installer"
+    private val channelName = "com.library.management/installer"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName).setMethodCallHandler { call, result ->
             if (call.method == "installApk") {
                 val filePath = call.argument<String>("filePath")
                 if (filePath != null) {
